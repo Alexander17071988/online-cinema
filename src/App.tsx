6a6ae26e4e5ca7b114169b7dc3b831f4d899d6ef
Alexander Zhmurkov <a.zhmurkov@mail.ru>
2024-09-23 21:21:22 +0300

    Исправил замечания от 23.09.2024

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import MovieList from './components/MovieList';
 import MovieCarousel from './components/MovieCarousel';
 import { Container, Typography } from '@mui/material'
 
+type TypographyVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle1' | 'subtitle2' | 'body1' | 'body2' | 'caption' | 'button' | 'overline';
+type TypographyAlign = 'inherit' | 'left' | 'center' | 'right' | 'justify';
+
+const groups: { fs: TypographyVariant; title: string; align?: TypographyAlign }[] = [
+    {
+        fs: 'h3',
+        title: 'Movie Theater',
+        align: 'center',
+    },
+    {
+        fs: 'h4',
+        title: 'Movie Carousel',
+    },
+    {
+        fs: 'h4',
+        title: 'All Movies',
+    }
+];
+
 const App: React.FC = () => {
+
     return (
         <Container>
-            <Typography variant='h3' align='center' gutterBottom>
-                Movie Theater
-            </Typography>
-
-            <Typography variant='h4' gutterBottom>
-                Movie Carousel
-            </Typography>
+            {groups.map(({ title, fs, align = 'left' }) => (
+                <Typography key={title} variant={fs} align={align} gutterBottom>
+                    {title}
+                </Typography>
+            ))}
             <MovieCarousel />
-
-            <Typography variant='h4' gutterBottom>
-                All Movies
-            </Typography>
             <MovieList />
         </Container>
     );
